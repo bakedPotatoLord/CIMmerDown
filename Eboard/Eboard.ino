2140f81127ac4d539d359b5c5173abb69a592f1b
bakedpotatolord <jeffy9745@gmail.com>
2025-09-01 14:25:17 -0600

    finalize encoder code

--- Eboard/Eboard.ino
+++ Eboard/Eboard.ino
@@ -1,87 +1,79 @@
 #include <Servo.h>
 
-const int esc1Pin      = 2;
-const int esc2Pin      = 3;
+#define esc1Pin 4
+#define esc2Pin 5
 
 const int powerLedPin  = 8;
 const int statusLedPin = 6;
 
 const int buzzerPin    = 7;
 
-const int encoderCLKPin  = 4; //green
-const int encoderDataPin  = 5; //yelow
+#define encoderCLKPin 2 //green
+#define encoderDataPin 3 //yelow
 
-const int encoderSwPin = 9;
+#define throttleMin 1250
+#define throttleMax 1750
+#define throttleNone 1500
+#define throttleDeadband 4 // out of 50
 
-const int throttleMin  = 1250;
-const int throttleMax  = 1750;
-const int throttleNone = 1500;
-
-volatile long encoderCount = 1500;
-int lastEncoderB = LOW;
+volatile int encoderPos = 0;
 Servo esc1, esc2;
+
+void interuptHandler(){
+  int clkState  = digitalRead(encoderCLKPin);
+  int dataState = digitalRead(encoderDataPin);
+  if (clkState == dataState) encoderPos++;
+  else encoderPos--;
+}
 
 void setup() {
   esc1.attach(esc1Pin);
   esc2.attach(esc2Pin);
 
-  esc1.writeMicroseconds(throttleMin);
-  esc2.writeMicroseconds(throttleMin);
+  //for testing. remove for final code
+  esc1.writeMicroseconds(1550);
+  esc2.writeMicroseconds(1550);
   delay(2000);
-  esc1.writeMicroseconds(throttleMax);
-  esc2.writeMicroseconds(throttleMax);
+  esc1.writeMicroseconds(1450);
+  esc2.writeMicroseconds(1450);
   delay(2000);
-  esc1.writeMicroseconds(throttleMin);
-  esc2.writeMicroseconds(throttleMin);
-  delay(2000);
+  esc1.writeMicroseconds(throttleNone);
+  esc2.writeMicroseconds(throttleNone);
 
-  Serial.begin(9600);
+  Serial.begin(115200);
+  Serial.println("init");
 
-  pinMode(powerLedPin, OUTPUT);
-  digitalWrite(powerLedPin, HIGH);
-  pinMode(statusLedPin, OUTPUT);
-  pinMode(buzzerPin, OUTPUT);
-  pinMode(encoderSwPin, INPUT_PULLUP);
   pinMode(encoderCLKPin, INPUT_PULLUP);
-  pinMode(encoderDataPin, INPUT_PULLUP);
+  pinMode(encoderDataPin, INPUT);
 
-  attachInterrupt(digitalPinToInterrupt(encoderCLKPin), onEncoderAChange, RISING);
-  esc1.writeMicroseconds(throttleMin);
-  esc2.writeMicroseconds(throttleMin);
+  attachInterrupt(digitalPinToInterrupt(encoderCLKPin), interuptHandler, CHANGE);
 }
 
+
 void loop() {
-  noInterrupts();
-  long count = encoderCount;
-  interrupts();
+  encoderPos = constrain(encoderPos, -50, 50);
+  int pulse = map(encoderPos, -50, 50, throttleMin, throttleMax);
 
-  // Serial.println(count);
-
-  count = constrain(count, 0, 100);
-  int pulse = map(count, 0, 100, throttleMin, throttleMax);
+  if(abs(encoderPos) < throttleDeadband){
+    pulse = 0;
+  }
 
   esc1.writeMicroseconds(pulse);
   esc2.writeMicroseconds(pulse);
 
-  analogWrite(statusLedPin, map(pulse, throttleMin, throttleMax, 0, 255));
-
-  if (pulse >= throttleMax) tone(buzzerPin, 1000);
-  else noTone(buzzerPin);
-
-  if (digitalRead(encoderSwPin) == LOW) {
-    esc1.writeMicroseconds(throttleNone);
-    esc2.writeMicroseconds(throttleNone);
-    digitalWrite(powerLedPin, LOW);
-    delay(200);
-    digitalWrite(powerLedPin, HIGH);
-    delay(200);
-  }
-
-  delay(10);
+  // analogWrite(statusLedPin, map(pulse, throttleMin, throttleMax, 0, 255));
+  // if (pulse >= throttleMax) tone(buzzerPin, 1000);
+  // else noTone(buzzerPin);
+  // if (digitalRead(encoderSwPin) == LOW) {
+  //   esc1.writeMicroseconds(throttleNone);
+  //   esc2.writeMicroseconds(throttleNone);
+  //   digitalWrite(powerLedPin, LOW);
+  //   delay(200);
+  //   digitalWrite(powerLedPin, HIGH);
+  //   delay(200);
+  // }
+  Serial.println(encoderPos);
+  delay(50);
 }
 
-void onEncoderAChange() {
-  int bState = digitalRead(encoderDataPin);
-  encoderCount += (bState != lastEncoderB) ? 1 : -1;
-  lastEncoderB = bState;
-}
+
