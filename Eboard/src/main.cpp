a316fd415eb7627749c680ad382c83936d5bcf99
bakedpotatolord <jeffy9745@gmail.com>
2025-12-19 17:37:37 -0700

    increase polling freqency

--- Eboard/src/main.cpp
+++ Eboard/src/main.cpp
@@ -5,10 +5,13 @@
 #endif
 
 #include "Arduino.h"
+#include <avr/sleep.h>
+
+#define CYCLE_FREQ 20UL //clock frequency in Hz
+#define CYCLE_WAVELEN ( 1000UL / CYCLE_FREQ) //wavelength in ms
 
 //if REMOTE is not defined, assume reciever
 
-bool led =0;
 
 // ==========================================================
 // setup() — Runs once on startup
@@ -28,16 +31,26 @@ void setup() {
 // ==========================================================
 // loop() — Main control loop, runs continuously
 // ==========================================================
-void loop() {
-  #ifdef REMOTE
-    rfLoop();  // Initialize radio for remote control
-  #else
-    boardLoop();
-  #endif
 
-  led = !led;
-  digitalWrite(LED_BUILTIN, led);
-  // Serial.println("hello from the loop");
-  delay(1000);
+u32 lastLoop = 0;
+
+void loop() {
+  u32 now = millis();
+
+  if(now - lastLoop > CYCLE_WAVELEN){ 
+    Serial.println(now);
+    Serial.println(lastLoop);
+    lastLoop = millis();
+    #ifdef REMOTE
+      rfLoop();  // Initialize radio for remote control
+    #else
+      boardLoop();
+    #endif
+  }else{
+    set_sleep_mode(SLEEP_MODE_IDLE);
+    sleep_enable();
+    sleep_cpu();
+    sleep_disable();
+  }
 
 }
